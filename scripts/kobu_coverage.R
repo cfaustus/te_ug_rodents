426dca02e3493c633f89563dc34fb92079eb0ceb
laura-bergner <laura.bergner@glasgow.ac.uk>
2024-08-27 13:47:40 +0100

    kobuvirus coverage - normalised plots

--- scripts/kobu_coverage.R
+++ scripts/kobu_coverage.R
@@ -7,54 +7,66 @@ kobu_per_site<-read.table("/Users/laura/Dropbox/glasgow/github/te_ug_rodents/dat
 
 #TE data only - compare coverage between backgrounds/viral loads
 
-kobu_per_site %>%
+#calculate the normalised coverage per site
+
+norm_cov_per_site<-kobu_per_site %>%
+  mutate(norm_cov = coverage/Number.of.paired.end.reads..QT.)
+
+norm_cov_per_site %>%
   filter(mapper == "bowtie2") %>%
   filter(expt == "TE") %>%
   filter(type == "dedup") %>%
   filter(Background != "p6") %>%
-  ggplot(aes(x=site,y=coverage,fill=Background))+
+  ggplot(aes(x=site,y=norm_cov,fill=Background))+
   geom_col(position="dodge")+
   facet_wrap(~Sample_id)
 
-kobu_per_site %>%
+TE_coverage_sep<-norm_cov_per_site %>%
   filter(mapper == "bowtie2") %>%
   filter(expt == "TE") %>%
   filter(type == "dedup") %>%
   filter(Background != "p6") %>%
-  ggplot(aes(x=site,y=coverage,fill=as.character(Viral.load)))+
+  ggplot(aes(x=site,y=norm_cov,fill=as.character(Viral.load)))+
   geom_col(position="dodge")+
-  facet_wrap(~Sample_id)
+  facet_wrap(~Sample_id)+
+  ylab("Normalised coverage")+
+  guides(fill=guide_legend(title="Viral load"))
 
-TE_coverage<-kobu_per_site %>%
+ggsave("/Users/laura/Dropbox/glasgow/github/te_ug_rodents/figures/kobuvirus_TE_coverage_separate.pdf")
+
+TE_coverage<-norm_cov_per_site %>%
   filter(mapper == "bowtie2") %>%
   filter(expt == "TE") %>%
   filter(type == "dedup") %>%
   filter(Background != "p6") %>%
-  ggplot(aes(x=site,y=coverage))+
+  ggplot(aes(x=site,y=norm_cov))+
   geom_col(position="dodge")+
-  facet_grid(as.character(Viral.load)~Background)
+  facet_grid(as.character(Viral.load)~Background)+
+  ylab("Normalised coverage")
 
 ggsave("/Users/laura/Dropbox/glasgow/github/te_ug_rodents/figures/kobuvirus_TE_coverage.pdf")
 
 #TE vs shotgun
 
-cov_TE_shotgun<-kobu_per_site %>%
+cov_TE_shotgun<-norm_cov_per_site %>%
   filter(mapper == "bowtie2") %>%
   filter(type == "dedup") %>%
   filter(Background != "p6") %>%
-  ggplot(aes(x=site,y=coverage))+
+  ggplot(aes(x=site,y=norm_cov))+
   geom_col(position="dodge")+
-  facet_grid(expt~Background)
+  facet_grid(expt~Background)+
+  ylab("Normalised coverage")
 
 ggsave("/Users/laura/Dropbox/glasgow/github/te_ug_rodents/figures/kobuvirus_TE_shotgun_coverage.pdf")
 
 #TE vs shotgun with p6
 
-cov_TE_shotgun_p6<-kobu_per_site %>%
+cov_TE_shotgun_p6<-norm_cov_per_site %>%
   filter(mapper == "bowtie2") %>%
   filter(type == "dedup") %>%
-  ggplot(aes(x=site,y=coverage))+
+  ggplot(aes(x=site,y=norm_cov))+
   geom_col(position="dodge")+
-  facet_grid(expt~Background)
+  facet_grid(expt~Background)+
+  ylab("Normalised coverage")
 
 ggsave("/Users/laura/Dropbox/glasgow/github/te_ug_rodents/figures/kobuvirus_TE_shotgun_coverage_p6.pdf")
